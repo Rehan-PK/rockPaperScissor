a92aa2de1262927c9c47b3f63076162f467ef741
Rehan Ali <rehan.to@gmail.com>
2023-11-08 19:16:05 +0500

    game() function implemented

--- file.js
+++ file.js
@@ -1,23 +1,23 @@
 const choices = ['Rock', 'Paper', 'Scissors'];
+let playerSelection;
+let computerSelection;
+// const playerSelection = "rock".toLowerCase();
 
 function getComputerChoice() {
     return choices[Math.floor(Math.random() * 3)].toLowerCase();
 }
 
-// const playerSelection = "rock".toLowerCase();
-
-let playerSelection = prompt("Input your option ?").toLowerCase();
-if (playerSelection === 'rock' || playerSelection === 'paper' || playerSelection === 'scissors') {
-    playerSelection;
-} else {
-    alert("You entered incorrect option, please refresh page to play again");
+function getPlayerSelection() {
+    playerSelection = prompt("Input your option ?").toLowerCase();
+    if (playerSelection === 'rock' || playerSelection === 'paper' || playerSelection === 'scissors') {
+        return playerSelection;
+    } else {
+        alert("You entered incorrect option, please refresh page to play again");
+        return 0;
+    }
 }
 
-const computerSelection = getComputerChoice();
-
 function playRound(playerSelection, computerSelection) {
-    // code away
-
     // play a single round
     if (playerSelection === 'rock') {
         if (computerSelection === 'rock') {
@@ -46,9 +46,35 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-function game() {
+function game() {    
+    // play a 5 round game
+    let playerScore = 0;
+    let computerScore = 0;
+    for (let i = 0; i < 5; i++) {
+        playerSelection = getPlayerSelection();
+        computerSelection = getComputerChoice();
+        
+        let result = playRound(playerSelection, computerSelection);
+        let winner = result.substring(0, 7);
+
+        if (winner === 'You Win') {
+            playerScore++;
+        } else if (winner === 'You Los') {
+            computerScore++;
+        } else {
+            true;
+        }
+        console.log(result);
+        console.log(`Player Score: ${playerScore}, Computer Score: ${computerScore}`);
+    }
+    
+    // report winner / looser at end
+    if (playerScore > computerScore) {
+        return "Congratulations! You have defeated the computer & won!";
+    } else if (playerScore < computerScore) {
+        return "Alas! You have lost to the cyborg :("
+    } else {
+        return "You are as smarter as a computer - result is a tie!"
+    }
 
 }
-
-console.log(playRound(playerSelection, computerSelection));
-
